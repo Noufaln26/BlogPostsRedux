e6e88a28385b125813a55ff2cfb06437c59468a6
Bincy Mariyam <bincymariyam97@gmail.com>
2022-04-11 22:04:10 +0530

    Added rest all and increse by amount

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useSelector,  useDispatch} from 'react-redux';
-import { increment,decrement } from './counterSlice';
+import { increment,decrement,reset, incrementbyamount } from './counterSlice';
 
 function Counter() {
     const count=useSelector((state)=>state.counter.count)
     const dispatch=useDispatch();
+    const [amount, setAmount] = useState(0)
+    const addValue=Number(amount)|| 0;
+
+    const resetAll =()=>{
+        setAmount(0)
+        dispatch(reset())
+
+    }
     
   return ( <>
     <div>{count}</div>
     <div>
         <button onClick={()=>dispatch(increment())}>+</button>
         <button onClick={()=>dispatch(decrement())}>-</button>
+        <div>
+        <button onClick={()=>dispatch(reset())}>Reset</button>
+        </div>
+        <input type="text" value={amount} onChange={(e)=>setAmount(e.target.value)} />
+        <button onClick={()=>dispatch(incrementbyamount(addValue))}>Add</button>
+        <button onClick={resetAll}>Reset All</button>
+       
+        
     </div>
     </>
   )
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -15,9 +15,15 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
+    reset: (state) =>{
+        state.count=0;
+    },
+    incrementbyamount: (state,action) =>{
+        state.count += action.payload
+    }
   },
 
 });
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement,reset,incrementbyamount} = counterSlice.actions;
 
 export default counterSlice.reducer;
