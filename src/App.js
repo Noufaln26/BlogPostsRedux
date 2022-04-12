97c871d96a136dbd93e128e54d4f416a962f013f
Noufal N <noufaln26@gmail.com>
2022-04-12 17:46:51 +0530

    added index.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-
 import './App.css';
-import Counter from './features/counter/Counter';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Redux using Redux toolkit </h1>
-        <Counter/>
-      </header>
+      
     </div>
   );
 }
